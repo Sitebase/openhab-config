211a3f823262840193600ed46f78b20dd6a031d3
Wim <wim@sitebase.be>
2015-08-08 16:04:35 +0200

    add way to manage and deploy custom icons

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,5 @@
 # Deploy new configuration to my home server OpenHAB instance
 rsync -azP config/ wim@192.168.1.117:/home/wim/applications/openhab/configurations/
+
+# Sync custom assets
+rsync -azP assets/images/ wim@192.168.1.117:/home/wim/applications/openhab/webapps/images/
