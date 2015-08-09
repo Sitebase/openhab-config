1bf21de55a48686ce723e238a6bea4fd0cffecf9
Wim <wim@sitebase.be>
2015-08-09 12:37:28 +0200

    add sunrise and sunset items

--- provision.sh
+++ provision.sh
@@ -19,6 +19,8 @@ unzip $BASE_DIR/addons.zip -d $BASE_DIR/addons
 
 echo "Install addons"
 cp $BASE_DIR/addons/org.openhab.action.mail-1.7.0.jar $BASE_DIR/openhab/addons
+cp $BASE_DIR/addons/org.openhab.action.astro-1.7.0.jar $BASE_DIR/openhab/addons
+cp $BASE_DIR/addons/org.openhab.binding.astro-1.7.0.jar $BASE_DIR/openhab/addons
 cp $BASE_DIR/addons/org.openhab.binding.exec-1.7.0.jar $BASE_DIR/openhab/addons
 cp $BASE_DIR/addons/org.openhab.binding.mqtt-1.7.0.jar $BASE_DIR/openhab/addons
 cp $BASE_DIR/addons/org.openhab.binding.mqttitude-1.7.0.jar $BASE_DIR/openhab/addons
